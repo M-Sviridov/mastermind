74d88b4e5cb416165733bd366aa2878abaf18194
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-18 14:53:25 +1000

    Refactor code and add method to check if human correctly guessed

--- computer_play.rb
+++ computer_play.rb
@@ -6,12 +6,13 @@ require 'pry'
 # class representing the case where the human is the guesser in Mastermind
 class ComputerPlay
   include Display
-  attr_reader :secret_code, :human_code, :human_guesses
+  attr_reader :secret_code, :human_guesses
+  attr_accessor :human_code
 
   def initialize
     @secret_code = random_code
     @human_guesses = []
-    @human_code = human_guess
+    @human_code = nil
   end
 
   def random_code
@@ -27,29 +28,29 @@ class ComputerPlay
   def human_guess
     loop do
       print 'Enter your combination of 4 digits (between 1 and 6): '
-      code = gets.chomp.chars.map(&:to_i)
-      if valid_guess?(code)
-        add_human_guess(code)
-        return code
+      @human_code = gets.chomp.chars.map(&:to_i)
+      if valid_guess?
+        add_human_guess
+        return @human_code
       end
 
       puts "\nIncorrect combination, make sure the respect the conditions."
     end
   end
 
-  def valid_guess?(code)
-    code.all? { |digit| digit.between?(1, 6) } && code.length == 4
+  def valid_guess?
+    @human_code.all? { |digit| digit.between?(1, 6) } && @human_code.length == 4
   end
 
-  def add_human_guess(code)
-    human_guesses << code
+  def add_human_guess
+    human_guesses << @human_code
   end
 
   def max_guesses?
     human_guesses.length == 12
   end
-end
 
-comp = ComputerPlay.new
-binding.pry
-# comp.human_guess
+  def correct_guess?
+    @human_code == secret_code
+  end
+end
