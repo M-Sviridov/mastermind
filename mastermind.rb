a63c5afef0aacdad17c5cba4ca68d80940c237f2
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-18 16:23:22 +1000

    Show all the past human guesses on screen

--- mastermind.rb
+++ mastermind.rb
@@ -1,13 +1,24 @@
+# frozen_string_literal: true
+
 require './computer_play'
 require './display'
 
 comp = ComputerPlay.new
-puts comp.reveal_code(comp.secret_code)
+secret_code = comp.secret_code
 
-comp.human_guess until comp.max_guesses? || comp.correct_guess?
+until comp.max_guesses? || comp.correct_guess?
+  comp.human_guess
+  comp.human_guesses.each do |guess|
+    puts "\n"
+    comp.reveal_code(guess)
+    puts "\n"
+  end
+end
 
 if comp.max_guesses?
   puts 'You lost! You used your 12 guesses.'
 elsif comp.correct_guess?
-  puts "You won, you guessed the code #{comp.reveal_code(comp.secret_code)}"
+  puts 'You won, you guessed the code: '
+  comp.reveal_code(secret_code)
+  puts "\nCongratulations!"
 end
