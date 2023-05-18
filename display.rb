9e0c25041ff0913f7a57bc2f55649fc19c89b078
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-18 14:06:40 +1000

    Create display module to represent each digit of the code as a color

---
+++ display.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# module that displays certain information for the game
+module Display
+  def colors_code(number)
+    {
+      1 => "\e[41m  1  \e[0m ",
+      2 => "\e[42m  2  \e[0m ",
+      3 => "\e[43m  3  \e[0m ",
+      4 => "\e[44m  4  \e[0m ",
+      5 => "\e[45m  5  \e[0m ",
+      6 => "\e[46m  6  \e[0m "
+    }[number]
+  end
+end
