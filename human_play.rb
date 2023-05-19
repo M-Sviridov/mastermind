200b2d423d47f6a16bf4be7a3f18fcf81c040615
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-19 19:12:35 +1000

    Moved 'printing' methods to Display module and 'logic' methods to GameLogic module

--- human_play.rb
+++ human_play.rb
@@ -2,12 +2,15 @@
 
 require './display'
 require './colorable'
+require './game_logic'
 
 # class representing the case where the human is the guesser in Mastermind
 class HumanPlay
-  include Display
-  using Colorable
   attr_reader :secret_code, :human_guesses, :total_turns, :human_code
+
+  include Display
+  include GameLogic
+  using Colorable
 
   def initialize
     @total_turns = 1
@@ -20,7 +23,7 @@ class HumanPlay
   end
 
   def start
-    until max_guesses? || correct_guess?
+    until max_guesses?(human_guesses) || correct_guess?(human_code)
       human_guess
       puts "\n"
       reveal_code(human_code)
@@ -28,12 +31,12 @@ class HumanPlay
       puts "\n"
     end
 
-    if max_guesses?
+    if max_guesses?(human_guesses)
       puts "\nYou lost! You used your 12 guesses."
       puts "The correct code was:\n\n"
       reveal_code(secret_code)
       puts ''
-    elsif correct_guess?
+    elsif correct_guess?(human_code)
       puts "\nCongratulations! You correctly guessed the secret code."
     end
   end
@@ -42,11 +45,6 @@ class HumanPlay
     Array.new(4) { rand(1..6) }
   end
 
-  def reveal_code(code)
-    code.each do |digit|
-      print colors_code digit
-    end
-  end
 
   def human_guess
     loop do
@@ -66,25 +64,7 @@ class HumanPlay
     human_code.all? { |digit| digit.between?(1, 6) } && human_code.length == 4
   end
 
-  def reveal_clues
-    exact_matches.times do
-      print clues_code 1
-    end
-
-    correct_numbers.times do
-      print clues_code 2
-    end
-  end
-
   def add_human_guess
     human_guesses << human_code
-  end
-
-  def max_guesses?
-    human_guesses.length == 12
-  end
-
-  def correct_guess?
-    human_code == secret_code
   end
 end
