af57c21e96e6d47a3ea6edb444969014cce5783b
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-20 15:30:31 +1000

    Create main file to launch the Mastermind game

---
+++ main.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require './display'
+require './colorable'
+require './human_play'
+require './computer_play'
+require './instructions'
+require './game'
+
+Game.new.start
