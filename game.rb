84d3e5868e273b72a972d8bfc12f30ee6a8d76b3
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-20 15:29:00 +1000

    Check that only game options are to input 1 or 2

--- game.rb
+++ game.rb
@@ -10,17 +10,29 @@ require './instructions'
 class Game
   include Instructions
   include Display
+  using Colorable
+
+  def initialize
+    @option = nil
+  end
 
   def start
     puts instructions
     game_option
+    launch_game
   end
 
   def game_option
-    option = gets.chomp.to_i
-    HumanPlay.new if option == 1
-    ComputerPlay.new if option == 2
+    loop do
+      @option = gets.chomp.to_i
+      return @option if @option == 1 || @option == 2
+
+      puts "\nPlease enter either 1 or 2.".fg_color(:aurora1)
+    end
+  end
+
+  def launch_game
+    HumanPlay.new if @option == 1
+    ComputerPlay.new if @option == 2
   end
 end
-
-Game.new.start
